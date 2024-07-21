987b9bbaa91823f0bb94e4d1dddc9cb34d244284
Dudu Akiva <duduke@gmail.com>
2024-07-21 10:57:12 +0300

    First commit of a set of scripts to configure a workstation to operate correctly in environment where SSL inspection is performed by a proxy (MiTM): These include: 1. Creating a certificate bundle. 2. Detect and configure tools, libraries and frameworks (ex: aswcli, azcli, python-pip, git). 3. Export configurations to an external script for faster run on other machines.

---
+++ configure_tools.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+## This tool will try to detect common cli tools and will configure the Netskope SSL certificate bundle.
+
+# Check which shell environment is used (bash)
+get_shell(){
+    my_shell=$(echo $SHELL)
+    echo "Shell used is $my_shell"
+    if [[ $my_shell == *"bash"* ]]; then
+        shell=~/.bashrc
+    else
+        shell=~/.profile
+    fi
+}
+get_shell
+
+# Set Certificate bundle name and location
+read -p "Please provide certificate bundle name [netskope-cert-bundle.pem]: " certName
+certName=${certName:-netskope-cert-bundle.pem}
+read -p "Please provide certificate bundle location [~/netskope]: " certDir
+certDir=${certDir:-~/netskope}
+if [ ! -d "$certDir" ]; then
+  echo "$certDir does not exist."
+  echo "creating $certDir"
+  mkdir -p $certDir
+fi
+
+# Get tenant information to create certificate bundle
+read -p "Please provide full tenant name (ex: mytenant.eu.goskope.com): " tenantName
+read -p "Please provide tenant orgkey: " orgKey
+
+status_code=$(curl -k --write-out %{http_code} --silent --output /dev/null https://$tenantName/locallogin)
+
+if [[ "$status_code" -ne "307" ]] ; then
+  echo "Tenant Unreachable"
+  exit 1
+else
+  echo "Tenant Reachable"
+fi
+
+# Function to check if a command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Function to create or update certificate bundle
+create_cert_bundle() {
+  echo "Creating cert bundle"
+  curl -k "https://addon-$tenantName/config/ca/cert?orgkey=$orgKey" > $certDir/$certName
+  curl -k "https://addon-$tenantName/config/org/cert?orgkey=$orgKey" >> $certDir/$certName
+  curl -k -L "https://ccadb-public.secure.force.com/mozilla/IncludedRootsPEMTxt?TrustBitsInclude=Websites" >> $certDir/$certName
+}
+
+if [ -f "$certDir/$certName" ]; then
+  echo "$certName already exists in $certDir."
+  read -p "Recreate Certificate Bundle? (y/N) " -n 1 -r
+  echo    
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    create_cert_bundle
+  fi
+else
+  create_cert_bundle
+fi
+
+# Function to configure a tool with the certificate bundle
+configure_tool() {
+  local tool_name=$1
+  local env_var=$2
+  local check_command=$3
+  local post_command=$4
+
+  echo
+  if command_exists $check_command; then
+    echo "$tool_name is installed"
+    $check_command --version
+    if [[ -n "$env_var" ]]; then
+      if [[ ${!env_var} == "$certDir/$certName" ]]; then
+        echo "$tool_name already configured"
+      else
+        echo "export $env_var=\"$certDir/$certName\"" >> $shell
+        echo "$tool_name configured"
+        source $shell
+        echo "export $env_var=\"$certDir/$certName\"" >> configured_tools.sh
+      fi
+    fi
+    if [[ -n "$post_command" ]]; then
+      eval $post_command
+      echo "$post_command" >> configured_tools.sh
+    fi
+  else
+    echo "$tool_name is not installed"
+  fi
+}
+
+# This allows for later silent runs on other machines
+> configured_tools.sh
+
+# Configure tools
+configure_tool "Git" "GIT_SSL_CAPATH" "git" ""
+configure_tool "OpenSSL" "SSL_CERT_FILE" "openssl" ""
+configure_tool "cURL" "SSL_CERT_FILE" "curl" ""
+configure_tool "Python Requests Library" "REQUESTS_CA_BUNDLE" "" ""
+configure_tool "AWS CLI" "AWS_CA_BUNDLE" "aws" ""
+configure_tool "Google Cloud CLI" "" "gcloud" "gcloud config set core/custom_ca_certs_file $certDir/$certName"
+configure_tool "NodeJS Package Manager (NPM)" "" "npm" "npm config set cafile $certDir/$certName"
+configure_tool "NodeJS" "NODE_EXTRA_CA_CERTS" "node" ""
+configure_tool "Ruby" "SSL_CERT_FILE" "ruby" ""
+configure_tool "PHP Composer" "" "composer" "composer config --global cafile $certDir/$certName"
+configure_tool "GoLang" "SSL_CERT_FILE" "go" ""
+configure_tool "Azure CLI" "REQUESTS_CA_BUNDLE" "az" ""
+configure_tool "Python PIP" "REQUESTS_CA_BUNDLE" "pip3" ""
+configure_tool "Oracle Cloud CLI" "REQUESTS_CA_BUNDLE" "oci" ""
+configure_tool "Cargo Package Manager" "SSL_CERT_FILE" "cargo" ""
+configure_tool "Yarn" "" "yarnpkg" "yarnpkg config set httpsCaFilePath $certDir/$certName"
+
+# Check if Azure Storage Explorer exists
+echo
+if [ -d ~/.config/StorageExplorer/certs ]; then
+  echo "Azure Storage Explorer is installed"
+  cp "$certDir/$certName" ~/.config/StorageExplorer/certs
+  echo "Azure Storage Explorer configured"
+  echo "cp \"$certDir/$certName\" ~/.config/StorageExplorer/certs" >> configured_tools.sh
+else
+  echo "Azure Storage Explorer is not installed"
+fi
+
+# Adding a new tool
+# To add a new tool, use the `configure_tool` function with the appropriate parameters.
+# Example:
+# configure_tool "Tool Name" "ENV_VAR_NAME" "check_command" "post_command"
+# - tool_name: The name of the tool (for display purposes)
+# - env_var: The environment variable to set (if applicable)
+# - check_command: The command to check if the tool is installed (usually the tool's executable name)
+# - post_command: Any additional configuration command needed after setting the environment variable (can be empty if not needed)
+#
+# Example for adding a hypothetical tool "MyTool":
+# configure_tool "MyTool" "MYTOOL_CA_CERTS" "mytool" "mytool config set cafile $certDir/$certName"
---
+++ configure_tools_windows.cmd
@@ -0,0 +1,224 @@
+@echo off
+:: This tool will try to detect common cli tools and will configure the Netskope SSL certificate bundle.
+
+:: Set Certificate bundle name and location
+set /p certName="Please provide certificate bundle name [netskope-cert-bundle.pem]:"
+if "%certName%"=="" set certName=netskope-cert-bundle.pem
+
+set /p certDir="Please provide certificate bundle location [C:\netskope]:"
+if "%certDir%"=="" set certDir=C:\netskope
+
+if not exist "%certDir%" (
+    echo %certDir% does not exist.
+    echo Creating %certDir%
+    mkdir "%certDir%"
+)
+
+:: Get tenant information to create certificate bundle
+set /p tenantName="Please provide full tenant name (ex: mytenant.eu.goskope.com):"
+set /p orgKey="Please provide tenant orgkey:"
+
+:: Check tenant reachability
+curl -k --write-out "%%{http_code}" --silent --output NUL https://%tenantName%/locallogin > temp.txt
+set /p status_code=<temp.txt
+del temp.txt
+
+if "%status_code%" NEQ "307" (
+    echo Tenant Unreachable
+    exit /b 1
+) else (
+    echo Tenant Reachable
+)
+
+:: Create or update certificate bundle
+set certBundleExists=0
+if exist "%certDir%\%certName%" (
+    echo %certName% already exists in %certDir%.
+    set /p recreate="Recreate Certificate Bundle? (y/n): "
+    if /i "%recreate%"=="y" set certBundleExists=1
+) else (
+    set certBundleExists=1
+)
+
+if %certBundleExists%==1 (
+    echo Creating cert bundle
+    curl -k "https://addon-%tenantName%/config/ca/cert?orgkey=%orgKey%" > "%certDir%\%certName%"
+    curl -k "https://addon-%tenantName%/config/org/cert?orgkey=%orgKey%" >> "%certDir%\%certName%"
+    curl -k -L "https://ccadb-public.secure.force.com/mozilla/IncludedRootsPEMTxt?TrustBitsInclude=Websites" >> "%certDir%\%certName%"
+)
+
+:: Tools configuration (add more tools here as needed)
+
+:: Initialize configured tools file
+echo @echo off > configured_tools.bat
+
+echo.
+call :command_exists git
+if %ERRORLEVEL% EQU 0 call :configure_tool git "git config --global http.sslCAInfo" "git config --global http.sslCAInfo" "git config --global http.sslCAInfo %certDir%\%certName%"
+
+echo.
+call :command_exists openssl
+if %ERRORLEVEL% EQU 0 call :configure_tool openssl "openssl version -a" "setx SSL_CERT_FILE" "setx SSL_CERT_FILE %certDir%\%certName%"
+
+echo.
+call :command_exists curl
+if %ERRORLEVEL% EQU 0 call :configure_tool curl "curl --version" "setx SSL_CERT_FILE" "setx SSL_CERT_FILE %certDir%\%certName%"
+
+echo.
+set REQUESTS_CA_BUNDLE=
+for /f "tokens=*" %%P in ('python -m requests') do (
+    if "%%P"=="built on:" set REQUESTS_CA_BUNDLE=%%P
+)
+if "%REQUESTS_CA_BUNDLE%"=="%certDir%\%certName%" (
+    echo Python Requests Already configured
+) else (
+    setx REQUESTS_CA_BUNDLE "%certDir%\%certName%"
+    echo Python Requests Library Configured
+    echo setx REQUESTS_CA_BUNDLE "%certDir%\%certName%" >> configured_tools.bat
+)
+
+echo.
+call :command_exists aws
+if %ERRORLEVEL% EQU 0 call :configure_tool aws "aws --version" "setx AWS_CA_BUNDLE" "setx AWS_CA_BUNDLE %certDir%\%certName%"
+
+echo.
+call :command_exists gcloud
+if %ERRORLEVEL% EQU 0 (
+    echo Google Cloud CLI is installed
+    gcloud --version
+    gcloud config set core/custom_ca_certs_file %certDir%\%certName%
+    echo Google Cloud CLI Configured
+    echo gcloud config set core/custom_ca_certs_file %certDir%\%certName% >> configured_tools.bat
+) else (
+    echo Google Cloud CLI is not installed
+)
+
+echo.
+call :command_exists npm
+if %ERRORLEVEL% EQU 0 (
+    echo "NodeJS Package Manager (NPM) is installed"
+    npm --version
+    npm config set cafile %certDir%\%certName%
+    echo "NodeJS Package Manager (NPM) Configured"
+    echo npm config set cafile %certDir%\%certName% >> configured_tools.bat
+) else (
+    echo "NodeJS Package Manager (NPM) is not installed"
+)
+
+echo.
+call :command_exists node
+if %ERRORLEVEL% EQU 0 call :configure_tool node "node --version" "setx NODE_EXTRA_CA_CERTS" "setx NODE_EXTRA_CA_CERTS %certDir%\%certName%"
+
+echo.
+call :command_exists ruby
+if %ERRORLEVEL% EQU 0 call :configure_tool ruby "ruby --version" "setx SSL_CERT_FILE" "setx SSL_CERT_FILE %certDir%\%certName%"
+
+echo.
+call :command_exists composer
+if %ERRORLEVEL% EQU 0 (
+    echo PHP Composer is installed
+    composer --version
+    composer config --global cafile %certDir%\%certName%
+    echo PHP Composer Configured
+    echo composer config --global cafile %certDir%\%certName% >> configured_tools.bat
+) else (
+    echo PHP Composer is not installed
+)
+
+echo.
+call :command_exists go
+if %ERRORLEVEL% EQU 0 call :configure_tool go "go --version" "setx SSL_CERT_FILE" "setx SSL_CERT_FILE %certDir%\%certName%"
+
+echo.
+call :command_exists az
+if %ERRORLEVEL% EQU 0 call :configure_tool az "az --version" "setx REQUESTS_CA_BUNDLE" "setx REQUESTS_CA_BUNDLE %certDir%\%certName%"
+
+echo.
+call :command_exists pip
+if %ERRORLEVEL% EQU 0 call :configure_tool pip "pip --version" "setx REQUESTS_CA_BUNDLE" "setx REQUESTS_CA_BUNDLE %certDir%\%certName%"
+
+echo.
+call :command_exists oci
+if %ERRORLEVEL% EQU 0 call :configure_tool oci "oci --version" "setx REQUESTS_CA_BUNDLE" "setx REQUESTS_CA_BUNDLE %certDir%\%certName%"
+
+echo.
+call :command_exists cargo
+if %ERRORLEVEL% EQU 0 (
+    echo Cargo Package Manager is installed
+    cargo --version
+    set SSL_CERT_FILE=
+    for /f "tokens=*" %%P in ('cargo --version') do (
+        if "%%P"=="built on:" set SSL_CERT_FILE=%%P
+    )
+    if "%SSL_CERT_FILE%"=="%certDir%\%certName%" (
+        echo Cargo Package Manager Already configured 1/2
+    ) else (
+        setx SSL_CERT_FILE "%certDir%\%certName%"
+        echo setx SSL_CERT_FILE "%certDir%\%certName%" >> configured_tools.bat
+    )
+    set GIT_SSL_CAPATH=
+    for /f "tokens=*" %%P in ('cargo --version') do (
+        if "%%P"=="built on:" set GIT_SSL_CAPATH=%%P
+    )
+    if "%GIT_SSL_CAPATH%"=="%certDir%\%certName%" (
+        echo Cargo Package Manager Already configured 2/2
+    ) else (
+        setx GIT_SSL_CAPATH "%certDir%\%certName%"
+        echo setx GIT_SSL_CAPATH "%certDir%\%certName%" >> configured_tools.bat
+    )
+    echo Cargo Package Manager configured
+) else (
+    echo Cargo Package Manager is not installed
+)
+
+echo.
+call :command_exists yarn
+if %ERRORLEVEL% EQU 0 (
+    echo Yarn is installed
+    yarn --version
+    yarn config set cafile %certDir%\%certName%
+    echo Yarn Configured
+    echo yarn config set cafile %certDir%\%certName% >> configured_tools.bat
+) else (
+    echo Yarn is not installed
+)
+
+:: Function to check if a command exists
+:command_exists
+where %1 > NUL 2>&1
+if %ERRORLEVEL% EQU 0 (
+    exit /b 0
+) else (
+    exit /b 1
+)
+
+:: Function to configure tools
+:configure_tool
+:: %1 - Tool name
+:: %2 - Command to retrieve the current configuration
+:: %3 - Command to set the new configuration
+:: %4 - Command to log configuration
+echo %~1 is installed
+%~1 --version
+set toolConfigured=0
+for /f "tokens=*" %%P in ('%~2') do set toolConfigured=%%P
+if "%toolConfigured%"=="%certDir%\%certName%" (
+    echo %~1 Already configured
+) else (
+    %~3 "%certDir%\%certName%"
+    echo %~1 Configured
+    echo %~1 Configured
+    echo %~4 >> configured_tools.bat
+)
+exit /b 0
+:: How to add a new tool:
+:: 1. Add a call to :command_exists followed by the tool name (e.g., "call :command_exists mytool").
+:: 2. If the tool is found (ERRORLEVEL is 0), call :configure_tool with the following parameters:
+::    - Tool name (e.g., "mytool")
+::    - Command to retrieve the current configuration (e.g., "mytool config --global cafile")
+::    - Command to set the new configuration (e.g., "mytool config --global cafile")
+::    - Command to log configuration (e.g., "mytool config --global cafile %certDir%\%certName%" >> configured_tools.bat)
+:: Example:
+:: echo.
+:: call :command_exists mytool
+:: if %ERRORLEVEL% EQU 0 call :configure_tool mytool "mytool config --global cafile" "mytool config --global cafile" "mytool config --global cafile %certDir%\%certName%" >> configured_tools.bat
